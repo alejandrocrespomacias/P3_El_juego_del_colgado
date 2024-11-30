22628411a81d049d95589bc436b79e46d9c0234f
alejandrocrespomacias <128037138+alejandrocrespomacias@users.noreply.github.com>
2024-11-30 20:13:48 +0100

    Commit5
    Scanner para leer la entrada del usuario
    Leer el numero de jugadores
    Validar que haya al menos 2 jugadores

--- src/El_juego_del_colgado/El_juego_del_colgado.java
+++ src/El_juego_del_colgado/El_juego_del_colgado.java
@@ -7,7 +7,24 @@ public class El_juego_del_colgado {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		Scanner scanner = new Scanner(System.in);
 
+		
+		System.out.println("¡Bienvenidos al juego del Colgado!");
+
+		
+		System.out.println("¿Cuántos jugadores van a jugar? (mínimo 2): ");
+		int numJugadores = scanner.nextInt();
+		scanner.nextLine();
+		
+		while (numJugadores < 2) {
+			System.out.println("¡Debe haber al menos 2 jugadores! Inténtelo de nuevo:");
+			numJugadores = scanner.nextInt();
+			scanner.nextLine(); 
+			
+			}
 		
 		
 	}
