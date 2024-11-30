55f3de571d58fbd89bff84972e2cc0d7b9e7e3aa
alejandrocrespomacias <128037138+alejandrocrespomacias@users.noreply.github.com>
2024-11-30 20:10:51 +0100

    Commit4
    Importo el Scanner

--- src/El_juego_del_colgado/El_juego_del_colgado.java
+++ src/El_juego_del_colgado/El_juego_del_colgado.java
@@ -1,10 +1,15 @@
 package El_juego_del_colgado;
 
+import java.util.Scanner;
+
+
 public class El_juego_del_colgado {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		
 	}
 
 }
